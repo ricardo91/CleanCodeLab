bed1675b5b6edcc26f7c2fde05e1032397d73764
Ricardo Arias <leonardoricardo.arias12@gmail.com>
2024-10-05 22:03:19 +0200

    tarea 1.5
    Refactorizar el método primes.

---
+++ src/tasks/primes_ok.ts
@@ -0,0 +1,19 @@
+// 👓 https://github.com/LabsAdemy/CleanCodeLab/tree/NAME/src/tasks
+/* eslint-disable no-magic-numbers */
+
+export function getPrimeFactors(limit: number): number[] {
+    const SMALLEST_PRIME = 2;
+    const NULL_REMAINDER = 0;
+    const primeFactors = [];
+    let testingFactor = SMALLEST_PRIME;
+    while (limit >= SMALLEST_PRIME) {
+      if (limit % testingFactor === NULL_REMAINDER) {
+        primeFactors.push(testingFactor);
+        limit /= testingFactor;
+      } else {
+        testingFactor++;
+      }
+    }
+    return primeFactors;
+  }
+  
